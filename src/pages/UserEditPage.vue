54883781e59d8a90c956ad6ce6d29bbcddddb960
zcnovice <2739650633@qq.com>
2025-06-28 16:16:13 +0800

    feat(layout): ts语法错误小修改(增加断言)+创建搜索返回页面

--- src/pages/UserEditPage.vue
+++ src/pages/UserEditPage.vue
@@ -14,18 +14,18 @@
     </van-form>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
 const editUser = ref({
-    editKey: route.query.editKey,
-    currentValue: route.query.currentValue,
-    editName: route.query.editName,
+    editKey: route.query.editKey as string,
+    currentValue: route.query.currentValue as string,
+    editName: route.query.editName as string,
 });
 
-const onSubmit = (values) => {
+const onSubmit = (values : Record<string, any>) => {
     //todo 把editKey, currentValue, editName 提交的后台
     console.log('onSubmit',values)
 };
